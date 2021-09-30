321ea45726a9c33fc0fc15abfdc89310908233cd
EdwinJOrtizCampos <edwinjavierortizcamposORCA1@HOTMAIL.COM>
2021-09-30 17:51:34 +0100

    Cuarta versión de Kata01_42

--- src/main/java/Kata2/Histogram.java
+++ src/main/java/Kata2/Histogram.java
@@ -2,22 +2,27 @@
 @Author Edwin Ortiz Campos
 @version 1.0
 */
+
+/*
+Comentario importante: debido a cómo empecé la kata, creando de antemano
+una clase Histogram, para esta versión los cambios son nulos.
+*/
 package Kata2;
 
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-public class Histogram {
-    private final int[] data;
+public class Histogram<T> {
+    private final T[] data;
     
-    public Histogram(int[] data){
+    public Histogram(T[] data){
         this.data = data;
     }
 
-    public Map<Integer, Integer> getHistogram() {
-        HashMap<Integer,Integer> histogram = new HashMap<>();
-        for(int key : data){
+    public Map<T, Integer> getHistogram() {
+        HashMap<T,Integer> histogram = new HashMap<>();
+        for(T key : data){
             histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
         }
         return histogram;
@@ -27,9 +32,9 @@ public class Histogram {
         @Override
     public String toString(){
         String result = "";
-        Iterator<Map.Entry<Integer, Integer>> iterator = getHistogram().entrySet().iterator();
+        Iterator<Map.Entry<T, Integer>> iterator = getHistogram().entrySet().iterator();
         while(iterator.hasNext()){
-            Map.Entry<Integer, Integer> next = iterator.next();
+            Map.Entry<T, Integer> next = iterator.next();
             result += next.getKey() + "==>" + next.getValue() + "\n";
         }
         return result;
